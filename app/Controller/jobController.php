4277569f5b863e09aa2d9c11393e169c648960d3
Emmanuel Lucio Urbina <emmanuelluur@gmail.com>
2018-12-17 20:57:28 -0600

    validaciones

--- app/Controller/jobController.php
+++ app/Controller/jobController.php
@@ -2,20 +2,41 @@
 namespace App\Controller;
 
 use App\Model\Job;
+use Respect\Validation\Validator;
 
 class JobController extends BaseController
 {
     public function getJob($request)
     {
+        //  var_dump($request->getParsedBody()); show data from views
+        $responseMessage = null;
+        $cssClass = null;
+
         if ($request->getMethod() == 'POST') {
-            $datos = $request->getParsedBody();
-            $job = new Job;
-            $job->title = $datos['title'];
-            $job->description = $datos['description'];
-            $job->visible = true;
-            $job->save();
+            $jobValidator = Validator::key('title', Validator::stringType()->notEmpty())
+                ->key('description', Validator::stringType()->notEmpty());
+            try {
+                //code...
+                $jobValidator->assert($request->getParsedBody()); // valida datos de entrada
+                $job = new Job;
+                $job->title = $request->getParsedBody()['title'];
+                $job->description = $request->getParsedBody()['description'];
+                $job->visible = true;
+                $job->save();
+                $responseMessage = "Saved";
+                $cssClass = 'alert alert-success';
+            } catch (\Exception $e) {
+                //throw $th;
+                $cssClass = 'alert alert-danger';
+                $responseMessage = $e->getMessage();
+            }
+            
+
         }
         //  include_once "../views/addJobs.php";
-        return $this->RenderHtml('addJobs.twig');
+        return $this->RenderHtml('addJobs.twig',[
+            'responseMessage'=>$responseMessage,
+            'cssClass' => $cssClass
+        ]);
     }
 }
--- app/Controller/projectController.php
+++ app/Controller/projectController.php
@@ -2,20 +2,41 @@
 namespace App\Controller;
 
 use App\Model\Project;
+use Respect\Validation\Validator;
 
 class ProjectController extends BaseController
 {
     public function getProject($request)
     {
+        //  var_dump($request->getParsedBody()); show data from views
+        $responseMessage = null;
+        $cssClass = null;
+
         if ($request->getMethod() == 'POST') {
-            $datos = $request->getParsedBody();
-            $project = new Project;
-            $project->title = $datos['title'];
-            $project->description = $datos['description'];
-            $project->visible = true;
-            $project->save();
+            $projectValidator = Validator::key('title', Validator::stringType()->notEmpty())
+                ->key('description', Validator::stringType()->notEmpty());
+            try {
+                //code...
+                $projectValidator->assert($request->getParsedBody());
+                $project = new Project;
+                $project->title = $request->getParsedBody()['title'];
+                $project->description = $request->getParsedBody()['description'];
+                $project->visible = true;
+                $project->save();
+
+                $responseMessage = "Saved";
+                $cssClass = 'alert alert-success';
+            } catch (\Exception $e) {
+                //throw $th;
+                $cssClass = 'alert alert-danger';
+                $responseMessage = $e->getMessage();
+            }
+
         }
         //  include_once "../views/addProjects.php";
-        return $this->RenderHtml('addProjects.twig');
+        return $this->RenderHtml('addProjects.twig',[
+            'responseMessage'=>$responseMessage,
+            'cssClass' => $cssClass
+        ]);
     }
 }
